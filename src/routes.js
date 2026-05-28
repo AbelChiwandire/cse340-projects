0193f5a460474ea7ae158377b3b1224de72f9a8e
AbelChiwandire <achiwandire1@byupathway.edu>
2026-05-28 15:37:23 +0200

    feat: implemented client-side and server-side form validation

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-
 import { showHomePage } from './controllers/index.js';
 import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm } from './controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
+import { organizationValidation } from './controllers/organizations.js';
 
 const router = express.Router();
 
@@ -17,6 +17,6 @@ router.get('/projects/:id', showProjectDetailsPage);
 router.get('/organizations/:id', showOrganizationDetailsPage);
 router.get('/categories/:id', showCategoryPage);
 router.get('/new-organization', showNewOrganizationForm);
-router.post('/new-organization', processNewOrganizationForm);
+router.post('/new-organization', organizationValidation, processNewOrganizationForm);
 
 export default router;
